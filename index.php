9acfc01f5a04b309b95c1d844aab43b4e291491a
swewalka-tgm <swewalka@student.tgm.ac.at>
2022-05-31 20:26:07 +0200

    linux

--- index.php
+++ index.php
@@ -10,28 +10,36 @@
     $link_poll = "./src/poll.php";
     $link_logout = "./src/inc/logout.php";
     require("src/inc/header.inc.php");
-    require("src/inc/navbar.inc.php");
+    
     ?>
-    <div class="container-fluid pt-5 pb-0 text-center">
-        <h1>Willkommen zurück, <?php echo $username?>!</h1>
-        <div class="col-lg-6 mx-auto">
-            <p class="lead mt-4 mb-4">
-                Das ist dein Dashboard. Hier kannst du zwischen dem Chat und dem Poll auswählen. Auch dein Konto kannst du hier verwalten und anpassen.
-            </p>
-        </div>
-        <div class="container-xxl d-lg-flex justify-content-evenly dashboard_choice">
-            <a href="<?php echo $link_chat ?>">
-                <div class="container px-5">
-                    <img src="./media/Screenshot_20220412_183352.png" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" loading="lazy">
-                </div>
-            </a>
-            <a href="<?php echo $link_poll ?>">
-                <div class="container px-5">
-                    <img src="https://place-hold.it/1080x720" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" loading="lazy">
-                </div>
-            </a>
-        </div>
-    </div>
+    <div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-chat100">
+				<div class="col-md-12">
+					<span class="login100-form-title">Dashboard</span>
+							<a href="<?php echo $link_chat ?>">
+								<div class = "text-center">
+									<div class="js-tilt" data-tilt>
+										<div class="container px-5">
+											<img src="https://www.superoffice.de/globalassets/home-com-website/resources/articles/visuals/chat-benefits/redesign_live-chat.png" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="400" loading="lazy">
+										</div>
+									</div>
+								</div>
+							</a>
+							<a href="<?php echo $link_poll ?>">
+								<div class = "text-center">
+									<div class="js-tilt" data-tilt>
+										<div class="container px-5">
+											<img src="https://www.voxco.com/wp-content/uploads/2021/10/How-are-polls-conducted1.jpg" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="400" loading="lazy">
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+	
     
     <?php
     require("src/inc/footer.inc.php");
