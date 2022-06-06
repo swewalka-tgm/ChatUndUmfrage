eec7d7c8fb9b4c5e12588dabf49e176865a14c4a
jszuecs-tgm <jszuecs@student.tgm.ac.at>
2022-06-06 22:43:49 +0200

    new

--- src/create_poll.php
+++ src/create_poll.php
@@ -20,55 +20,68 @@
     
 
 ?>
-<form class="container-lg border border-primary chat_wrapper rounded w-50 mt-5" id="form" method="post" action="create_polldb.php">
-    <h2 class="mt-3">Bitte fülle Folgende Felder aus:</h2>
 
-    <!--Row-->
-    <div class="row">
 
-    <div class="mb-3 mt-3"></div>
+<div class="limiter">
+      <div class="container-login100">
+        <div class="wrap-chat100">
+        <a class="btn logout" href="<?php echo $link_logout?>">Log Out</a>
+            <span class="login100-form-title">
+              Chat
+            </span>
+            <div class="container-lg rounded text-center">
+            <form class="container-lg border chat_wrapper rounded" id="form" method="post" action="create_polldb.php">
+                    <!--Row-->
+                    <div class="row">
 
-    <!--Question-->
-    <div class="col">
-        <div class="input-group">
-        <label for="question" class="input-group-text">Question</label>
-        <input type="text" class="form-control" id="question" placeholder="Question" name="question" required />
+                    <div class="mb-3 mt-3"></div>
+
+                    <!--Question-->
+                    <div class="col">
+                        <div class="input-group">
+                        <label for="question" class="input-group-text">Question</label>
+                        <input type="text" class="form-control" id="question" placeholder="Question" name="question" required />
+                        </div>
+                    </div>
+
+                    <!--Antwort1-->
+                    <div class="col">
+                        <div class="input-group"> 
+                        <label for="anwser1" class="input-group-text">Answer1</label>
+                        <input type="text" class="form-control" id="anwser1" placeholder="Answer1" name="anwser1" required />
+                        </div>
+                    </div>
+
+                    <!--Antwort2-->
+                    <div class="col">
+                        <div class="input-group"> 
+                        <label for="anwser2" class="input-group-text">Answer2</label>
+                        <input type="text" class="form-control" id="anwser2" placeholder="Answer2" name="anwser2" required />
+                        </div>
+                    </div>
+
+                    </div>
+                    
+                    <!--Row-->
+                    <div class="row">
+
+                    <div class="mb-3 mt-3"></div>
+
+                    <!--Buttons-->
+
+                    <div class="col">
+                        <a href="<?php echo $link_poll ?>"><button type="submit" name="submit" class="btn btn-primary mt-3">Cancel</button></a>
+                    </div>
+
+                    <div class="col">
+                        <a href="<?php echo $link_create_polldb ?>"><button type="submit" name="submit" class="btn btn-primary mt-3">Create</button></a>
+                    </div>
+                    </div>
+                </form>
+            </div>
         </div>
-    </div>
-
-    <!--Antwort1-->
-    <div class="col">
-        <div class="input-group"> 
-        <label for="anwser1" class="input-group-text">Answer</label>
-        <input type="text" class="form-control" id="anwser1" placeholder="Answer" name="anwser1" required />
-        </div>
-    </div>
-
-    <!--Antwort2-->
-    <div class="col">
-        <div class="input-group"> 
-        <label for="anwser2" class="input-group-text">Answer</label>
-        <input type="text" class="form-control" id="anwser2" placeholder="Answer" name="anwser2" required />
-        </div>
-    </div>
-
-    </div>
-    
-    <!--Row-->
-    <div class="row">
-
-    <div class="mb-3 mt-3"></div>
-
-    <!--Buttons-->
-
-    <div class="col">
-        <a href="<?php echo $link_create_polldb ?>"><button type="submit" name="submit" class="btn btn-primary mt-3">Create</button></a>
-    </div>
-
-    <div class="col">
-        <a href="<?php echo $link_poll ?>"><button type="submit" name="submit" class="btn btn-primary mt-3">Cancel</button></a>
-    </div>
-
-
-    </div>
-</form>
+      </div>
+  </div>
+    <?php
+    require("inc/footer.inc.php");
+?>
